8720e24eaa269a09e7c07d73028d2ee395e7d47d
Prashant Anand <106450463+PrashantAnand03@users.noreply.github.com>
2024-08-26 15:15:43 +0530

    first commit

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,41 +1,70 @@
+import './loader.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import NewsItems from './NewsItems';
+import { BallTriangle } from 'react-loader-spinner'; // Importing BallTriangle loader
 
 const NewsBoard = ({ category }) => {
   const [news, setNews] = useState([]);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true); // State for loading
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
         const url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+        console.log(`Fetching URL: ${url}`);
+        
         const response = await axios.get(url);
-        console.log(response.data.articles); // Log the response to verify
-        if (response.data.articles) {
+        
+        console.log('API Response:', response);
+        console.log('Articles:', response.data.articles);
+      
+        if (response.data && response.data.articles) {
           setNews(response.data.articles); // Update state with fetched news
         } else {
           setNews([]);
         }
       } catch (err) {
         console.error('Error fetching news:', err);
+        
         if (err.response) {
-          setError(`HTTP error! Status: ${err.response.status}`);
+          console.error('Error Response:', err.response);
+          setError(`HTTP error! Status: ${err.response.status}. Message: ${err.response.data.message}`);
         } else if (err.request) {
+          console.error('Error Request:', err.request);
           setError('No response received from the server');
         } else {
+          console.error('Error Message:', err.message);
           setError(err.message);
         }
+      } finally {
+        setLoading(false); // Set loading to false once the data is fetched
       }
     };
-
+    
+    
     fetchNews();
   }, [category]);
 
   return (
     <div className="news-board d-flex flex-wrap">
-      {error ? (
+      {loading ? (
+        <div className="loading-container">
+          <BallTriangle
+            height={100}
+            width={100}
+            radius={5}
+            color="#4fa94d"
+            ariaLabel="ball-triangle-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+          <p>Loading...</p>
+        </div>
+      ) : error ? (
         <p>{error}</p>
       ) : (
         news.length > 0 ? (
